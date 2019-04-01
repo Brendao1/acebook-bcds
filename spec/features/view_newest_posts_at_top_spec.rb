d3aaf6ebd0d7df158978f8458782adda0704fa97
“Brenda” <odonnell.brenda@gmail.com>
2019-04-01 12:28:01 +0100

    refactored view_new_posts_at_top to include sign_up_bob helper, this resolves merge conflict with David and Clare's branch

--- spec/features/view_newest_posts_at_top_spec.rb
+++ spec/features/view_newest_posts_at_top_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature "view newest posts at the top", type: :feature do
     Post.create(message: "abc")
     sleep(0.1)
     Post.create(message: "def")
+    sign_up_bob
     visit '/posts'
     expect("def").to appear_before("abc")
   end
@@ -14,14 +15,8 @@ RSpec.feature "view newest posts at the top", type: :feature do
     sleep(0.1)
     Post.create(message: "def")
     visit '/'
-    user_email = "bob@bob.com"
-    user_password = "1234567890"
-    click_link "Sign up"
-      fill_in "Email", with: user_email
-      fill_in "Password", with: user_password
-      fill_in "Password confirmation", with: user_password
-      click_button "Sign up"
-      expect("def").to appear_before("abc")
+    sign_up_bob
+    expect("def").to appear_before("abc")
   end
 
 end
