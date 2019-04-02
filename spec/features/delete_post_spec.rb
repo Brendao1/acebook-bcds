1b1d6a919b68e3e0e19c151792f08e57f8910b66
David Thai <dafuloth@users.noreply.github.com>
2019-04-02 15:53:13 +0100

    Wrote TDD test 'Delete not available on post by others'

--- spec/features/delete_post_spec.rb
+++ spec/features/delete_post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "User can delete post", type: :feature do
-  scenario "User can delete their own post" do
+  scenario "User can delete post" do
     user = User.create(email: "a@a.com", password: "1234567890")
     post = Post.create(message: "first message", user_id: user.id)
     visit "/"
@@ -11,5 +11,23 @@ RSpec.feature "User can delete post", type: :feature do
     click_button "Log in"
     click_link "Delete"
     expect(page).not_to have_content("first message")
+  end
+
+  scenario "Delete not available on post by others" do
+    user1 = User.create(email: "a@a.com", password: "1234567890")
+    post1 = Post.create(message: "first message", user_id: user1.id)
+
+    user2 = User.create(email: "b@b.com", password: "1234567890")
+
+    visit "/"
+    click_link "Login"
+    fill_in "Email", with: "b@b.com"
+    fill_in "Password", with: "1234567890"
+    click_button "Log in"
+
+    expect(page).not_to have_content("Delete")
+
+
+
   end
 end
