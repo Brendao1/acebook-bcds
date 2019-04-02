063b36ade3ba6cbe004620a4a634055563527693
“Brenda” <odonnell.brenda@gmail.com>
2019-04-02 11:49:40 +0100

    adds new feature test to view post: user can see their own posts

--- spec/features/view_post_spec.rb
+++ spec/features/view_post_spec.rb
@@ -7,4 +7,16 @@ RSpec.feature "View posts", type: :feature do
         user = sign_up_bob
         expect(page).to have_content("abcdefg")
     end
+    scenario "users's own posts are viewed when user signs up" do
+        user = sign_up_bob
+        click_link "Logout"
+        bobs_post = Post.create(message: "bob's post", user_id: user.id)
+        visit '/'
+        click_link "Login"
+        fill_in "Email", with: "bob@bob.com"
+        fill_in "Password", with: "1234567890"
+        click_button "Log in"
+        expect(page).to have_content("bob's post")
+    end
+
 end
