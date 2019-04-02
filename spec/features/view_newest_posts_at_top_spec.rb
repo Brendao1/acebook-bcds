8354724d4b828d88804242a0cc93686bb4dfd4b9
“Brenda” <odonnell.brenda@gmail.com>
2019-04-02 11:25:53 +0100

    updates view_newest_posts_at_the_top by creating a user, and assigning that user's id to the Posts.

--- spec/features/view_newest_posts_at_top_spec.rb
+++ spec/features/view_newest_posts_at_top_spec.rb
@@ -12,9 +12,10 @@ RSpec.feature "view newest posts at the top", type: :feature do
   end
 
   scenario "User will see newest posts at the top of the index page" do
-    Post.create(message: "abc")
+    user = User.create(email: 'a@a.com', password: '1234567890')
+    Post.create(message: "abc", user_id: user.id)
     sleep(0.1)
-    Post.create(message: "def")
+    Post.create(message: "def", user_id: user.id)
     visit '/'
     sign_up_bob
     expect("def").to appear_before("abc")
