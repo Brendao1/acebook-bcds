36f9785e30510c6512ca0194f90dbd894a5fa2a9
CLARE PINDER <CLARE.PINDER@LIVE.CO.UK>
2019-04-02 11:19:26 +0100

    fixed test - view newest posts at the top

--- spec/features/view_newest_posts_at_top_spec.rb
+++ spec/features/view_newest_posts_at_top_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.feature "view newest posts at the top", type: :feature do
   scenario "User will see newest posts at the top of the post page" do
-    Post.create(message: "abc")
+    user = User.create(email: 'a@a.com', password: '1234567890')
+    Post.create(message: "abc", user_id: user.id)
     sleep(0.1)
-    Post.create(message: "def")
+    Post.create(message: "def", user_id: user.id)
     sign_up_bob
     visit '/posts'
     expect("def").to appear_before("abc")
