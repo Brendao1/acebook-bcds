58638434e3c1cbf374a6004eb51ddbf58bdaf11b
“Brenda” <odonnell.brenda@gmail.com>
2019-04-02 11:35:27 +0100

    updates view post spec by adding the another user's id to another_user's post

--- spec/features/view_post_spec.rb
+++ spec/features/view_post_spec.rb
@@ -1,17 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "View posts", type: :feature do
-    user_email = "bob@bob.com"
-    user_password = "1234567890"
     scenario "posts are viewed when user signs up" do
-        post = Post.create(message: "abcdefg")
-        visit "/"
-        click_link "Sign up"
-        fill_in "Email", with: user_email
-        fill_in "Password", with: user_password
-        fill_in "Password confirmation", with: user_password
-        length =  User.all.length
-        click_button "Sign up"
+        another_user = User.create(email: 'another@a.com', password: "1234567890")
+        post = Post.create(message: "abcdefg", user_id: another_user.id)
+        user = sign_up_bob
         expect(page).to have_content("abcdefg")
     end
 end
