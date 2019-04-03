17434aa6c0a8a544c419462dbbe9644df44927f8
Soroush Khosravi <sorush.kh68@gmail.com>
2019-04-03 12:06:37 +0100

    Now, we are able to create likes in the database.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,15 @@
 class LikesController < ApplicationController
-  def new
+  before_action :find_post
+
+  def create
+    @post.likes.create(user_id: current_user.id)
+    redirect_to root_url
+  end
+
+
+
+  private
+  def find_post
+    @post = Post.find(params[:post_id])
   end
 end
