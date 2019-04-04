4508565a1e4ae7597c94ba8f6428e50d240a4370
Soroush Khosravi <sorush.kh68@gmail.com>
2019-04-04 11:37:50 +0100

    The unit tset for the creation of a like is done.

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
+  describe "GET /new " do
+    it "We can create a like" do
+      user = User.create(email: "A@a.com", password: "123456789")
+      post = Post.create(message: "hey", user_id: user.id)
+      like = Like.create(user_id: user.id , post_id: post.id)
+      expect(Like.all[0].user_id).to eq user.id
+      expect(Like.all[0].post_id).to eq post.id
+    end
+  end
 
 end
