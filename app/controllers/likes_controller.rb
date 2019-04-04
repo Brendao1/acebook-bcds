4d678c9e706f4158fd5147c982bdc95873ae974e
David Thai <dafuloth@users.noreply.github.com>
2019-04-04 10:27:30 +0100

    mobbed  code review and revisions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,18 +8,19 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    Like.destroy(@like.ids) # This works. Compare to posts#destroy?
-    # @like.destroy #Doesn't work
+    #Like.destroy(@like.ids) # previous solution 
+    @like.destroy
     redirect_to root_url
   end
 
 private
   def find_post
-    @post = Post.find(params[:post_id])
+    @post = Post.find_by(id: params[:post_id])
   end
 
   def find_like
-    @like = Like.where(user_id: current_user.id, post_id: params[:post_id])
+    @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    # previous solution: @like = Like.where
     # ActiveRecords finder methods like 'where' return a list of matches.
     # In this case, our database entity relationships mean there can be only one
   end
